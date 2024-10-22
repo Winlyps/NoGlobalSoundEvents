351960e4d7cea893f74350ae571c3c869f97879d
Winlypse <nts3@o2.pl>
2024-10-22 18:02:23 +0200

    Initial commit

---
+++ src/main/kotlin/winlyps/noGlobalSoundEvents/NoGlobalSoundEvents.kt
@@ -0,0 +1,14 @@
+package winlyps.noGlobalSoundEvents
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class NoGlobalSoundEvents : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
